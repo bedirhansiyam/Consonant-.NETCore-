98bde96f64b2e2f889f174b718065f9118838d2e
Bedirhan <bedirhansiyam@gmail.com>
2023-03-21 20:17:25 +0400

    First commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Consonant;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Please enter a word : ");
+        string word = Console.ReadLine();
+        string regexId = @"[^aeiouAEIOU]{2}";
+        bool check;
+
+        Regex regex = new Regex(regexId);
+        
+        if(regex.IsMatch(word))
+            check = true;
+
+        else
+            check = false;
+
+        Console.WriteLine("");
+        Console.WriteLine("Result : " + check);
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# Consonant - .Net Core
+In this application, the user enters a word and the application checks for 2 consecutive consonants and returns *True* if any, otherwise returns *False* and prints it to the console.
+</br>
+
+*Sample input: Hello - patika - Happy* </br>
+*Sample output: True - False - True*
+
+</br>
+
+[Patika.dev](https://app.patika.dev/)
